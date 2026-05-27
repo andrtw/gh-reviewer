e99b079a91e6a4b8500e0bdbd27bc5cd0d72a1da
andrtw <andrea96.t@gmail.com>
2026-05-28 01:10:54 +0200

    Setup argparse for add and rm commands

---
+++ args.py
@@ -0,0 +1,42 @@
+import argparse
+
+import update_reviewer
+
+
+def setup_argparse():
+    parser = argparse.ArgumentParser(
+        description="Fuzzy find reviewers and add/remove them to/from a pull request",
+    )
+    subparsers = parser.add_subparsers(required=True)
+
+    __add_add_reviewer_parser(subparsers)
+    __add_remove_reviewer_parser(subparsers)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+def __add_add_reviewer_parser(subparsers: argparse._SubParsersAction):
+    add_subparser = subparsers.add_parser(
+        "add",
+        help="add reviewer(s)",
+    )
+    add_subparser.add_argument(
+        "number",
+        nargs="?",
+        help="pull request number. If empty, the reviewer(s) are added to the pull request that belongs to the current branch",
+    )
+    add_subparser.set_defaults(func=update_reviewer.add_reviewer)
+
+
+def __add_remove_reviewer_parser(subparsers: argparse._SubParsersAction):
+    rm_subparser = subparsers.add_parser(
+        "rm",
+        help="remove reviewer(s)",
+    )
+    rm_subparser.add_argument(
+        "number",
+        nargs="?",
+        help="pull request number. If empty, the reviewer(s) are removed from the pull request that belongs to the current branch",
+    )
+    rm_subparser.set_defaults(func=update_reviewer.rm_reviewer)
---
+++ update_reviewer.py
@@ -0,0 +1,11 @@
+from argparse import Namespace
+
+
+def add_reviewer(args: Namespace):
+    print("add")
+    pass
+
+
+def rm_reviewer(args: Namespace):
+    print("remove")
+    pass
